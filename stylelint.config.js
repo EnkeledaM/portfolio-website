143a080ced86969f9064f0c96d7ebc177f62790a
EnkeledaM <kelamustafaj@hotmail.com>
2025-09-12 15:35:26 +0200

    Center layout and minimize whitespace across pages

---
+++ stylelint.config.js
@@ -0,0 +1,15 @@
+// stylelint.config.js
+module.exports = {
+  rules: {
+    'block-no-empty': true,
+    'color-no-invalid-hex': true,
+    'declaration-block-no-duplicate-properties': true,
+    'font-family-no-duplicate-names': true,
+    'selector-pseudo-class-no-unknown': true,
+    'selector-pseudo-element-no-unknown': true,
+    'no-empty-source': true,
+    'no-extra-semicolons': true,
+    'string-no-newline': true,
+    'unit-no-unknown': true
+  }
+};
